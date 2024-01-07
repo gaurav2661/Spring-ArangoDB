1f3f38ad2597abdd5817ab1db60138f097ebc768
Gaurav Singh <gaurav.2661@gmail.com>
2024-01-07 17:24:22 +0530

    first commit

---
+++ src/main/java/com/arangodb/springsecurityarangodb/SpringSecurityArangodbApplication.java
@@ -0,0 +1,13 @@
+package com.arangodb.springsecurityarangodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityArangodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityArangodbApplication.class, args);
+	}
+
+}
